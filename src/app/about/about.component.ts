39310aed241811fd7b7a4a957ef973581c7bfb3a
Ogunniyi Olumuyiwa David <ogunniyo@sheridancollege.ca>
2025-10-15 00:19:47 -0400

    edit on info

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -14,7 +14,7 @@ export class AboutComponent implements OnInit, OnDestroy {
   info: Information = {
     name: "Muyiwa Davids",
     role: "Full Stack Developer",
-    skills: "React • Angular • REST APIs • SQL • Linux Systems Administration • Cloud Computing • Hands-On with Real-World Projects & Scalable Architecture | Computer Programming Student @ Sheridan College | Multiple Certification",
+    skills: "React • Angular • NodeJS• ExpressJS• REST APIs • PostgreSQL • SQL • Linux Systems Administration • Cloud Computing • Hands-On with Real-World Projects & Scalable Architecture | Computer Programming Student @ Sheridan College | Multiple Certification",
     describe: "Hi, My name is Olumuyiwa David Ogunniyi, currently a student of Computer programming at Sheridan College, I'm an innovative and detail-oriented junior Software Developer with a strong foundation in full-stack web development, system administration, and scalable application design. I specialize in building clean, responsive user experiences with Angular, React, TypeScript, and Material Design just like this one, complemented by hands-on experience in backend systems, database modeling, and Linux-based service configuration.\n" +
       "\n" +
       "My recent projects span a range of modern development challenges — from designing a minimalist Angular portfolio with custom routing and search filtering, to implementing secure services (FTP, NFS, Samba, Sendmail) in virtualized Linux environments. I’ve also led the development of normalized database models and RESTful APIs, contributing to efficient, maintainable codebases and cross-functional team collaboration.\n" +
